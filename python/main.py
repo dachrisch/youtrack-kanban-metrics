6b0c48c3af85b231fa6fceb2e28a0fa43a119d37
Christian Dähn <christian.daehn@gmail.com>
2016-10-24 16:33:36 +0200

    added percentile chart added history start date

--- python/main.py
+++ python/main.py
@@ -21,25 +21,56 @@ def main(arguments):
     cache = pyfscache.FSCache(arguments.cachedir, days=arguments.cacheage)
 
     yt = KanbanAwareYouTrackConnection('https://tickets.i.gini.net', arguments.username, arguments.password, cache)
-    now = datetime.datetime.now()
-    then = datetime.datetime.now() - datetime.timedelta(days=arguments.historyage)
+    if arguments.historyfrom:
+        now = datetime.datetime.strptime(arguments.historyfrom, '%Y-%m-%d')
+    else:
+        now = datetime.datetime.now()
+    then = now - datetime.timedelta(days=arguments.historyage)
 
     issues = yt.get_cycle_time_issues(arguments.project, 1000,
                                       history_range=(to_date_fetch_query(now), to_date_fetch_query(then)))
 
     base(issues, now, then)
 
-    chart_title = (arguments.project, (to_date_fetch_query(then), to_date_fetch_query(now)))
+    chart_title = '[%s] %s' % (arguments.project, (to_date_fetch_query(then), to_date_fetch_query(now)))
+
+    chart_filename = None
+    if arguments.savechart:
+        chart_filename = '%s_%s-%s.png' % (arguments.project, to_date_fetch_query(then), to_date_fetch_query(now))
     if arguments.chart == 'histogram':
-        histogram(issues, '[%s] %s' % chart_title,
-                  arguments.chart_file)
+        histogram(issues, chart_title, chart_filename)
     elif arguments.chart == 'control':
-        control_chart(issues, '[%s] %s' % chart_title,
-                      arguments.chart_file)
+        control_chart(issues, chart_title, chart_filename)
     elif arguments.chart == 'metrics':
         metrics(issues)
     elif arguments.chart == 'basic':
         pass
+    elif arguments.chart == 'percentile':
+        percentile(issues, chart_title, chart_filename)
+
+
+def percentile(issues, chart_title, chart_file):
+    import matplotlib.pyplot as plt
+    if args.chart_log:
+        plt.yscale('log')
+
+    cycletimes = [issue.cycle_time.days for issue in issues]
+    x_axis = (10, 25, 50, 75, 80, 90, 95, 99)
+    y_axis = [numpy.percentile(cycletimes, quantile) for quantile in x_axis]
+
+    axis = plt.subplot()
+    plt.plot(x_axis, y_axis)
+
+    plt.xlabel('Percentile')
+    plt.ylabel('Cycle Time [days]')
+    plt.title('Percentile chart for  %s' % chart_title)
+    plt.grid(True)
+    plt.grid(True, which='minor')
+
+    if chart_file:
+        plt.savefig('percentile_%s' % chart_file)
+    else:
+        plt.show()
 
 
 def control_chart(issues, chart_title, chart_file):
@@ -62,7 +93,7 @@ def control_chart(issues, chart_title, chart_file):
     plt.grid(True, which='minor')
 
     if chart_file:
-        plt.savefig(chart_file)
+        plt.savefig('control_%s' % chart_file)
     else:
         plt.show()
 
@@ -86,7 +117,7 @@ def histogram(issues, chart_title, chart_file):
     plt.axis([plot_bins[0], plot_bins[-1], 0, max(n) + 1])
     plt.grid(True)
     if chart_file:
-        plt.savefig(chart_file)
+        plt.savefig('histogram_%s' % chart_file)
     else:
         plt.show()
 
@@ -103,9 +134,9 @@ def base(issues, now, then):
     median_cycle_time = sorted(cycletimes)[len(cycletimes) // 2]
     max_cycle_time = numpy.max(cycletimes)
     min_cycle_time = numpy.min(cycletimes)
-    print 'min issue   : %s' % filter(lambda issue:issue.cycle_time.days == min_cycle_time,issues)[0]
-    print 'median issue: %s' % filter(lambda issue:issue.cycle_time.days == median_cycle_time,issues)[0]
-    print 'max issue   : %s' % filter(lambda issue:issue.cycle_time.days == max_cycle_time,issues)[0]
+    print 'min issue   : %s' % filter(lambda issue: issue.cycle_time.days == min_cycle_time, issues)[0]
+    print 'median issue: %s' % filter(lambda issue: issue.cycle_time.days == median_cycle_time, issues)[0]
+    print 'max issue   : %s' % filter(lambda issue: issue.cycle_time.days == max_cycle_time, issues)[0]
 
     mean_cycle_time = numpy.mean(cycletimes)
     print 'mean cycle time: %d days' % mean_cycle_time
@@ -136,11 +167,12 @@ if __name__ == '__main__':
     parser.add_argument('--cacheage', dest='cacheage', type=int, default=14, help='days before updating cache')
     parser.add_argument('--historyage', dest='historyage', default=90, type=int,
                         help='how many days to fetch (from now)')
+    parser.add_argument('--historyfrom', dest='historyfrom', help='where to start fetching (instead of "now")')
     parser.add_argument('--chart_log', dest='chart_log', action='store_true', default=False,
                         help='days before updating cache')
-    parser.add_argument('--savechart', dest='chart_file', default=None,
+    parser.add_argument('--savechart', dest='savechart', action='store_true', default=None,
                         help='save chart to file instead of showing it')
 
-    parser.add_argument('chart', choices=('histogram', 'control', 'metrics', 'basic'))
+    parser.add_argument('chart', choices=('histogram', 'control', 'metrics', 'basic', 'percentile'))
     args = parser.parse_args()
     main(args)
